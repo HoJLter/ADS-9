3c5e767dfa5e86be8184fd97e26a651190069b04
Ivan Suvorov <hojlter.work@gmail.com>
2026-05-26 16:40:12 +0300

    Add: std:: namespace

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@
 #include "tree.h"
 
 int main() {
-    cout << "n,getAll,getPerm1,getPerm2\n";
+    std::cout << "n,getAll,getPerm1,getPerm2\n";
 
-    srand(time(NULL));
+    std::srand(std::time(NULL));
 
     for (int n = 2; n <= 10; n++) {
-        vector<char> a;
+
+        std::vector<char> a;
         for (int i = 0; i < n; i++) {
             a.push_back('A' + i);
         }
@@ -21,24 +22,27 @@ int main() {
         PMTree tree(a);
 
         int maxPerm = factor(n);
-        int r = rand_r() % maxPerm + 1;
+        int r = std::rand() % maxPerm + 1;
 
-        auto start = high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
         getAllPerms(tree);
-        auto end = high_resolution_clock::now();
-        double tAll = duration<double, micro>(end - start).count();
+        auto end = std::chrono::high_resolution_clock::now();
+        double tAll =
+            std::chrono::duration<double, std::micro>(end - start).count();
 
-        start = high_resolution_clock::now();
+        start = std::chrono::high_resolution_clock::now();
         getPerm1(tree, r);
-        end = high_resolution_clock::now();
-        double t1 = duration<double, micro>(end - start).count();
+        end = std::chrono::high_resolution_clock::now();
+        double t1 =
+            std::chrono::duration<double, std::micro>(end - start).count();
 
-        start = high_resolution_clock::now();
+        start = std::chrono::high_resolution_clock::now();
         getPerm2(tree, r);
-        end = high_resolution_clock::now();
-        double t2 = duration<double, micro>(end - start).count();
+        end = std::chrono::high_resolution_clock::now();
+        double t2 =
+            std::chrono::duration<double, std::micro>(end - start).count();
 
-        cout << n << "," << tAll << "," << t1 << "," << t2 << "\n";
+        std::cout << n << "," << tAll << "," << t1 << "," << t2 << "\n";
     }
 
     return 0;
